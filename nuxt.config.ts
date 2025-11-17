d54de00a8b819b1a9a33f78e1d0b757462d6cb6b
kartikeyan <kartikeyan@traderz.com>
2025-11-17 10:14:06 +0530

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@ import tailwindcss from "@tailwindcss/vite";
 import Aura from '@primeuix/themes/aura';
 
 export default defineNuxtConfig({
+  ssr: false, 
+  app: {
+    baseURL: '/Chess-Verse/',   
+    buildAssetsDir: 'assets'
+  },
+  nitro: {
+    preset: 'github_pages'
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
